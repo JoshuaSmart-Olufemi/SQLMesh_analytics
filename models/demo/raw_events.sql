815fd01bf8f9e129bab1489aced66c5404365f70
Joshua Smart-Olufemi <joshuasmartolufemi@gmail.com>
2025-08-13 17:00:39 +0100

    latest_models

---
+++ models/demo/raw_events.sql
@@ -0,0 +1,14 @@
+MODEL (
+    name raw.events,
+    kind SEED (
+        path '../seeds/raw_events.csv'
+    ),
+    columns (
+        event_id INT,
+        user_id INT,
+        event_type TEXT,
+        event_timestamp TIMESTAMP,
+        revenue DECIMAL(10,2)
+    ),
+    grain event_id
+);
